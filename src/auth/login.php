aa17b70e96f43575f4f06a827b2788dad66714ce
DimasAjisaka <4anakcyber@gmail.com>
2022-04-13 17:41:20 +0700

    update login, footer, header, admin

--- src/auth/login.php
+++ src/auth/login.php
@@ -1,44 +1,64 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Login Aplikasi Pembayaran SPP</title>
-</head>
-<body>
-	<h3>Silahkan Login Menggunakan Username dan Password Anda</h3>
-	<hr/>
-	<form method="post" action="">
-		<table>
-			<tr>
-				<td>Username</td>
-				<td><input type="text" name="username" /></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td><input type="password" name="password" /></td>
-			</tr>
-			<tr>
-				<td>Role</td>
-				<td>
-					<select name="role">
-						<option value="" selected>- Pilih Role -</option>
-						<?php
-							include "../connection/connection.php";
-							$getRole = mysqli_query($conn, "SELECT * from roles");
-							while($data = mysqli_fetch_array($getRole)){
-						?>
-							<option value="<?php echo $data['role']; ?>"><?php echo $data['role']; ?></option>
-						<?php
-							}
-						?>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" value="Login" /></td>
-			</tr>
-		</table>
-	</form>
-	<?php include "../functions/get_login.php"; ?>
-	</body>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+    <title>Login</title>
+  </head>
+  <body style="background-color: #e3f2fd">
+    <div class="container h-100 mt-5">
+      <div class="row justify-content-center h-100 align-items-center">
+        <div class="col-md-6">
+          <div class="authincation-content">
+            <div class="row no-gutters">
+              <div class="col-xl-12">
+                <div class="card shadow p-5" style="border-radius: 26px">
+                  <div class="card-body">
+                    <div class="text-center mb-3">
+                      <img src="../images/logo-full.png" alt="" />
+                    </div>
+                    <h4 class="text-center mb-4">Sign in your account</h4>
+                    <form method="POST" action="">
+                      <div class="mb-3">
+                        <label class="mb-1"><strong>Username</strong></label>
+                        <input type="text" name="username" class="form-control" />
+                      </div>
+                      <div class="mb-3">
+                        <label class="mb-1"><strong>Password</strong></label>
+                        <input type="password" name="password" class="form-control" />
+                      </div>
+                      <div class="mb-3">
+                        <select class="form-select" name="role" aria-label="Default select example">
+                          <option value="" selected>Pilih Role</option>
+                          <?php
+														include "../connection/connection.php";
+														$getRole = mysqli_query($conn, "SELECT * from roles");
+														while($data = mysqli_fetch_array($getRole)) {
+													?>
+                          <option value="<?php echo $data['role']; ?>"><?php echo $data['role']; ?></option>
+                          <?php
+														}
+													?>
+                        </select>
+                      </div>
+                      <div class="d-grid gap-2 mt-5">
+                        <button style="border-radius: 10px; background-color: #2196f3; color: white" type="submit" class="btn p-2">Login</button>
+                      </div>
+                    </form>
+                    <?php include "../functions/get_login.php" ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
 </html>
--- src/header/header.php
+++ src/header/header.php
@@ -10,18 +10,50 @@
 ?>
 
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Aplikasi Pembayaran SPP</title>
-</head>
-<body>
-<h3>Aplikasi Pembayaran SPP</h3>
-<hr/>
-<a href="../index/admin.php">Dashboard</a> |
-<a href="../show/showdatauser.php">Data User</a> |
-<a href="../show/showdatawclass.php">Data Wali Kelas</a> |
-<a href="../show/showdatastudent.php">Data Siswa</a> |
-<a href="../show/showdataspp.php">Data SPP</a> |
-<a href="transaksi.php">Transaksi</a> |
-<a href="../auth/logout.php">Logout</a>
-<hr/>
+<html lang="en">
+  <head>
+    <!-- recuired meta tag -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+    <title>Aplikasi Pembayaran SPP</title>
+  </head>
+  <body style="background-color: #fafafa">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white m-3 shadow p-2" style="border-radius: 13px">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#" style="color: #2196f3">SppPayments</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="../index/admin.php">Dashboard</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../show/showdatauser.php">Data User</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../show/showdatawclass.php">Data Wali Kelas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../show/showdatastudent.php">Data Siswa</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../show/showdataspp.php">Data SPP</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="transaksi.php">Transaksi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../auth/logout.php">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </body>
+</html>
